edf50677d8a3481cdaba9a3acdfe8e627853ae28
Marais Rossouw <marais@touchpointglobal.com>
2016-01-20 13:58:18 +1100

    Somewhat there

--- src/assets/js/website.js
+++ src/assets/js/website.js
@@ -24,15 +24,8 @@
 			clearTimeout(id);
 		};
 
-	window.addEvent = function (object, type, callback) {
-		if (object == null || typeof(object) == 'undefined') return;
-		if (object.addEventListener) {
-			object.addEventListener(type, callback, false);
-		} else if (object.attachEvent) {
-			object.attachEvent("on" + type, callback);
-		} else {
-			object["on" + type] = callback;
-		}
+	Math.randomInRange = function(min, max) {
+		return min + (max - min) * Math.random();
 	};
 }());
 
