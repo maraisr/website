59be77d1ae26e522e09f056e665a3ca51d0e905c
Marais Rossouw <me@maraisr.com>
2016-05-17 22:07:17 +1000

    style: mosaic style change

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,4 +1,4 @@
 declare var require: any;
 
 const Mosaic = require('mosaic-js');
-new Mosaic(document.getElementById('vandal'), [86, 200, 148], [25, 52, 65], 250);
+new Mosaic(document.getElementById('vandal'), [183, 50, 20], [80, 30, 70], 264);
