f3b0164fc0058b98ba066b73c3c7b343104bc85a
Marais Rossouw <me@maraisr.com>
2016-08-29 22:17:48 +1000

    chore: navigation added

--- src/assets/js/instance/app.ts
+++ src/assets/js/instance/app.ts
@@ -1,5 +1,21 @@
 export default class App {
 	constructor() {
+		[...document.querySelectorAll('[scroll-to]')].map(v => ((document.getElementById(`content-${v.hash.replace(/^#/, '')}`)) ? v.addEventListener('click', (e: MouseEvent) => {
+			e.preventDefault() && e.cancelBubble;
 
+			(function scroll(to: number, duration: number) {
+				if (duration <= 0) return;
+				let dif = to - document.body.scrollTop;
+				let perTick = dif / duration * 10;
+
+				setTimeout(() => {
+					document.body.scrollTop = document.body.scrollTop + perTick;
+					if (document.body.scrollTop == to) return;
+					scroll(to, duration - 10);
+				}, 10);
+			})(document.getElementById(`content-${e.target.hash.replace(/^#/, '')}`).offsetTop, 600);
+
+			return false;
+		}) : void 0));
 	}
 }
