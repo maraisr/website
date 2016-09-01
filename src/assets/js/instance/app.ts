db59899c326de2ae01c2fc870f4503ea8ccccc58
Marais Rossouw <me@maraisr.com>
2016-09-02 09:59:51 +1000

    chore: almost ready, footer up

--- src/assets/js/instance/app.ts
+++ src/assets/js/instance/app.ts
@@ -5,5 +5,11 @@ export default class App {
 	constructor() {
 		new Nav(document.querySelectorAll('[scroll-to]'));
 		new Skills(document.getElementById('content-skills'));
+
+		// Imprint Toggle
+		[...document.getElementsByClassName('imprint-toggle')].forEach(btn => btn.addEventListener('click', () => {
+				document.body.classList.toggle('imprint-open');
+			})
+		);
 	}
 }
