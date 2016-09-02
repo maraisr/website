ee72aacf87dc466964dcde30a26e8962355776c7
Marais Rossouw <me@maraisr.com>
2016-09-02 11:32:56 +1000

    chore: tracking in and working with webpack

--- src/assets/js/instance/tracking.ts
+++ src/assets/js/instance/tracking.ts
@@ -2,26 +2,30 @@ declare var ga: any;
 
 export default class Tracking implements TrackingInterface {
 	constructor() {
-		ga('create', 'UA-47550066-2', 'auto');
-		ga('send', 'pageview');
+		if (!__DEV__) {
+			ga('create', 'UA-47550066-2', 'auto');
+			ga('send', 'pageview');
 
-		// Mailto links
-		[...document.querySelectorAll('[href^="mailto"]')].forEach((node: Element) => {
-			node.addEventListener('click', (e: MouseEvent) => this.track('Nav', 'click', 'mailto'));
-		});
+			// Mailto links
+			[...document.querySelectorAll('[href^="mailto"]')].forEach((node: Element) => {
+				node.addEventListener('click', (e: MouseEvent) => this.track('Nav', 'click', 'mailto'));
+			});
 
-		[... document.getElementsByClassName('social')].forEach((node: Element) => {
-			[...node.getElementsByTagName('a')].forEach((a: Element) => {
-				let title = a.getElementsByTagName('title')[0].textContent;
+			[... document.getElementsByClassName('social')].forEach((node: Element) => {
+				[...node.getElementsByTagName('a')].forEach((a: Element) => {
+					let title = a.getElementsByTagName('title')[0].textContent;
 
-				a.addEventListener('click', (e: MouseEvent) => {
-					this.track('Social', 'click', title);
-				});
-			})
-		});
+					a.addEventListener('click', (e: MouseEvent) => {
+						this.track('Social', 'click', title);
+					});
+				})
+			});
+		}
 	}
 
-	track(label, event, data) {
-		ga('send', 'event', label, event, data);
+	track(label: string, event: string, data: string): void {
+		if (!__DEV__) {
+			ga('send', 'event', label, event, data);
+		}
 	}
 }
---
+++ src/externs/ga.js
@@ -0,0 +1,5 @@
+/*
+ @const
+ */
+function ga() {
+}
