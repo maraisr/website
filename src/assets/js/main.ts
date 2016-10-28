0358e4492f5a5ef8c0f10c6edb611d61267027ae
Marais Rossouw <me@maraisr.com>
2016-10-28 23:12:23 +1000

    feature: started with some last fm stuff

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,11 +1,13 @@
 import Nav from './instance/nav';
 import Skills from './instance/skills';
 import Tracking from './instance/tracking';
+import FM from './instance/fm';
 
 const tracking = new Tracking();
 
 new Nav(document.querySelectorAll('[scroll-to]'), tracking);
 new Skills(document.getElementById('content-skills'));
+new FM(document.getElementById('last-fm'));
 
 // Imprint Toggle
 /*[...document.getElementsByClassName('imprint-toggle')].forEach(btn => btn.addEventListener('click', () => {
