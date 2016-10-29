9d1f52481d47ca7f3cdc7ac2272b7d131c70d153
Marais Rossouw <me@maraisr.com>
2016-10-29 10:03:11 +1000

    chore: updated the way it works (ts)

--- src/assets/js/instance/skills.ts
+++ src/assets/js/instance/skills.ts
@@ -1,32 +1,30 @@
-export default class Skills {
-	constructor(selectors: Element) {
-		let skillsZone = selectors.getElementsByClassName('skills'),
-			overZone = false;
+export default function (selectors: Element) {
+	let skillsZone = selectors.getElementsByClassName('skills'),
+		overZone = false;
 
-		[... selectors.getElementsByClassName('legend__item')].forEach(level => {
-			let levelAttr = level.getAttribute('level');
+	[... selectors.getElementsByClassName('legend__item')].forEach(level => {
+		let levelAttr = level.getAttribute('level');
 
-			((evt) => {
-				level.addEventListener('mouseover', evt);
-				level.addEventListener('touchstart', evt);
-			})(() => {
-				overZone = true;
-				skillsZone[0].className = `skills ${levelAttr}`;
-			});
-
-			level.addEventListener('mouseleave', () => overZone = false);
-			level.addEventListener('touchend', () => overZone = false);
+		((evt) => {
+			level.addEventListener('mouseover', evt);
+			level.addEventListener('touchstart', evt);
+		})(() => {
+			overZone = true;
+			skillsZone[0].className = `skills ${levelAttr}`;
 		});
 
-		((evt, ref) => {
-			ref.addEventListener('mouseout', evt);
-			ref.addEventListener('touchend', evt);
-		})(() => {
-			setTimeout(() => {
-				if (!overZone) {
-					skillsZone[0].className = 'skills';
-				}
-			}, 100);
-		}, selectors.getElementsByClassName('legend')[0]);
-	}
+		level.addEventListener('mouseleave', () => overZone = false);
+		level.addEventListener('touchend', () => overZone = false);
+	});
+
+	((evt, ref) => {
+		ref.addEventListener('mouseout', evt);
+		ref.addEventListener('touchend', evt);
+	})(() => {
+		setTimeout(() => {
+			if (!overZone) {
+				skillsZone[0].className = 'skills';
+			}
+		}, 100);
+	}, selectors.getElementsByClassName('legend')[0]);
 }
