5dfa92c8942bfd03ef92e04247c4c62b9e7c4ba9
Marais Rossouw <me@maraisr.com>
2018-01-17 07:42:18 +1000

    feat: much prettier now

--- src/assets/js/instance/skills.ts
+++ src/assets/js/instance/skills.ts
@@ -2,22 +2,22 @@ export default function(selectors: Element) {
 	let skillsZone = selectors.getElementsByClassName('skills'),
 		overZone = false;
 
-	Array.from(
-		selectors.getElementsByClassName('legend__item')
-	).forEach(level => {
-		let levelAttr = level.getAttribute('level');
+	Array.from(selectors.getElementsByClassName('legend__item')).forEach(
+		level => {
+			let levelAttr = level.getAttribute('level');
 
-		(evt => {
-			level.addEventListener('mouseover', evt);
-			level.addEventListener('touchstart', evt);
-		})(() => {
-			overZone = true;
-			skillsZone[0].className = `skills ${levelAttr}`;
-		});
+			(evt => {
+				level.addEventListener('mouseover', evt);
+				level.addEventListener('touchstart', evt);
+			})(() => {
+				overZone = true;
+				skillsZone[0].className = `skills ${levelAttr}`;
+			});
 
-		level.addEventListener('mouseleave', () => (overZone = false));
-		level.addEventListener('touchend', () => (overZone = false));
-	});
+			level.addEventListener('mouseleave', () => (overZone = false));
+			level.addEventListener('touchend', () => (overZone = false));
+		}
+	);
 
 	((evt, ref) => {
 		ref.addEventListener('mouseout', evt);
