eba7ec87e627d98b094ecf83bdbcbff50ccb3cee
Marais Rossouw <me@maraisr.com>
2018-07-02 20:56:12 +1000

    fix: scroll issue resolved

--- src/assets/js/instance/nav.ts
+++ src/assets/js/instance/nav.ts
@@ -4,37 +4,46 @@ interface AppElement extends Element {
 	hash?: string;
 }
 
-export default function(selectors: NodeList) {
-	Array.from(selectors).map(
-		(v: AppElement) =>
-			document.getElementById(`content-${v.hash.replace(/^#/, '')}`)
-				? v.addEventListener('click', (e: MouseEvent) => {
-						e.preventDefault();
+export default function(selectors: NodeList): void {
+	(Array.from(selectors) || [])
+		.map((buttonNode: AppElement) => ({
+			key: buttonNode.hash.replace(/^#/, ''),
+			node: buttonNode
+		}))
+		.filter(({ key }) => buttonHasContentNode(key))
+		.forEach(({ node, key }) => bindClickHandler(node, key));
+}
 
-						let clickTo: string = (<AppElement>(
-							e.target
-						)).hash.replace(/^#/, '');
+function bindClickHandler(element: Element, key: string): void {
+	element.addEventListener('click', (e: MouseEvent) => {
+		e.preventDefault();
 
-						(function scroll(to: number, duration: number) {
-							if (duration <= 0) return;
-							let dif = to - document.body.scrollTop;
-							let perTick = (dif / duration) * 10;
+		const contentNode: HTMLElement = <HTMLElement>(
+			document.getElementById(contentKey(key))
+		);
 
-							setTimeout(() => {
-								document.body.scrollTop += perTick;
-								if (document.body.scrollTop === to) return;
-								scroll(to, duration - 10);
-							}, 10);
-						})(
-							document.getElementById(`content-${clickTo}`)
-								.offsetTop,
-							600
-						);
+		Tracking.track('Nav', 'click', key);
 
-						Tracking.track('Nav', 'click', clickTo);
+		scroll(contentNode.offsetTop, 600);
+	});
+}
 
-						return false;
-				  })
-				: void 0
-	);
+function scroll(to: number, duration: number) {
+	if (duration <= 0) return;
+	const dif = to - window.pageYOffset;
+	const perTick = (dif / duration) * 10;
+
+	setTimeout(() => {
+		window.scrollTo(0, window.pageYOffset + perTick);
+		if (window.pageYOffset === to) return;
+		scroll(to, duration - 10);
+	}, 10);
+}
+
+function buttonHasContentNode(key: string): boolean {
+	return !!document.getElementById(contentKey(key));
+}
+
+function contentKey(key: string): string {
+	return `content-${key}`;
 }
