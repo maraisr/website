cce0e8b9a5ecab86ab910a05ba1f1c7885ef4671
Marais Rossouw <me@marais.co>
2021-12-21 15:25:12 +1000

    chore: reset for bongo

--- src/assets/js/defs/interfaces.d.ts
+++
@@ -1,8 +0,0 @@
-declare var __DEV__: boolean;
-declare var __BUILD__: string;
-declare var ga: any;
-declare var fbq: any;
-
-interface TrackingInterface {
-	track(label: string, event: string, data: string): void;
-}
--- src/externs/ga.js
+++
@@ -1,9 +0,0 @@
-/*
- @const
- */
-function ga() {}
-
-/*
- @const
- */
-function fbq() {}
