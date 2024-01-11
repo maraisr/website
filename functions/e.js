e40ee1aa9a1db99a0ae839d9220b9e49e18ac312
Marais Rossouw <me@marais.co>
2024-01-11 10:52:39 +1000

    chore: hide more in /e call

--- functions/e.js
+++ functions/e.js
@@ -1,5 +1,15 @@
-export function onRequestPost(ctx) {
-	const request = new Request(ctx.request);
+/** @param {{request: Request, waitUntil(p: Promise<any>): void}} ctx */
+export async function onRequestPost(ctx) {
+	const [u, r, w] = await ctx.request.json();
+	const request = new Request(ctx.request, {
+		body: JSON.stringify({
+			n: 'pageview',
+			d: 'marais.io',
+			u,
+			r,
+			w,
+		}),
+	});
 	request.headers.delete('cookie');
 	ctx.waitUntil(fetch('https://plausible.io/api/event', request));
 	return new Response(null, { status: 200 });
