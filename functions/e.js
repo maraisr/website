26a88dcaa1967127e30a089f7335a3c9dabf199a
Marais Rossouw <me@marais.co>
2025-04-10 07:21:51 +1000

    chore: lets try Seline

--- functions/e.js
+++ functions/e.js
@@ -1,16 +1,14 @@
 /** @param {{request: Request, waitUntil(p: Promise<any>): void}} ctx */
 export async function onRequestPost(ctx) {
-	const [u, r, w] = await ctx.request.json();
+	const [u, r] = await ctx.request.json();
 	const request = new Request(ctx.request, {
 		body: JSON.stringify({
-			n: 'pageview',
-			d: 'marais.io',
-			u,
-			r,
-			w,
+			token: 'f17175beee2851b',
+			pathname: u,
+			referrer: r,
 		}),
 	});
 	request.headers.delete('cookie');
-	ctx.waitUntil(fetch('https://plausible.io/api/event', request));
+	ctx.waitUntil(fetch('https://api.seline.so/s/e', request));
 	return new Response(null, { status: 200 });
 }
