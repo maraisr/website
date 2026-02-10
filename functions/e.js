bfa65ac28ac6f86d73ae5dd98d490308872095f2
Marais Rossouw <me@marais.co>
2026-02-10 11:34:10 +1000

    chore: just send to visitors for now

--- functions/e.js
+++ functions/e.js
@@ -2,34 +2,26 @@
 export async function onRequestPost(ctx) {
 	const user_ip = ctx.request.headers.get('CF-Connecting-IP');
 
-	const [u, r, w] = await ctx.request.json();
+	const [url, referrer] = await ctx.request.json();
 
-	const request = new Request(ctx.request, { body: null });
+	const request = new Request(ctx.request, {
+		method: 'POST',
+		body: JSON.stringify({
+			action: 'track',
+			payload: {
+				id: crypto.randomUUID(),
+				projectToken: '194e75d2-65cf-4402-a64c-67115b33d3e5',
+				url,
+				type: 'pageview',
+				referrer,
+				properties: {},
+			},
+		}),
+	});
 	request.headers.delete('cookie');
+	request.headers.set('x-forwarded-for', user_ip);
 
-
-	const plausible = new Request(request.clone(), {
-		body: JSON.stringify({
-			n: 'pageview',
-			d: 'marais.io',
-			u,
-			r,
-			w,
-		}),
-	});
-	plausible.headers.set('X-Forwarded-For', user_ip);
-	ctx.waitUntil(fetch('https://plausible.io/api/event', plausible));
-
-	const seline = new Request(request.clone(), {
-		body: JSON.stringify({
-			token: 'f17175beee2851b',
-			pathname: new URL(u).pathname,
-			referrer: r,
-		}),
-	});
-	seline.headers.set('Seline-IP', user_ip);
-	seline.headers.set('Seline-Country', ctx.request.headers.get('CF-IPCountry'));
-	ctx.waitUntil(fetch('https://api.seline.so/s/e', seline));
+	ctx.waitUntil(fetch('https://e.visitors.now/e', request));
 
 	return new Response(null, { status: 200 });
 }
